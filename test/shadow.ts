7cb70744db91c292e8358491061c1aaf078c39ce
Bilal Taherkheli <bilal76@gmail.com>
2021-02-14 13:12:40 +0100

    - could not use 'querySelectorDeep' directly - had to resort to plugin which could then be used as a custom locator strategy - shadow.ts now runs fine

--- test/shadow.ts
+++ test/shadow.ts
@@ -1,31 +1,19 @@
-// //TODO: Keeping TypeScript out of it for now, the task is to simply reference the npm package 'query-selector-shadow-dom' 'https://www.npmjs.com/package/query-selector-shadow-dom'
-// // and use 'querySelectorDeep' to locate nested shadow dom element on chorme://downloads
-// // run "npm test" to see a running test and this one failing
-// // >>>>>>>>>>>>>>>>>Get this :(
-// // Error: D:\github\wdio-shadow\node_modules\query-selector-shadow-dom\src\querySelectorDeep.js:6
-// // import { normalizeSelector } from './normalize';
-// // ^^^^^^
+import { locatorStrategy } from "query-selector-shadow-dom/plugins/webdriverio";
 
-// const querySelectorDeep = require('query-selector-shadow-dom');
+describe("Chrome downloads", () => {
+  before(() => {
+    browser.addLocatorStrategy("shadow", locatorStrategy);
+  });
 
-// describe("Chrome downloads", () => {
-//   it("shadow dom element can be located", () => {
+  it("shadow dom element can be located", () => {
+    browser.url("chrome://downloads/");
+    const moreAction = browser.custom$("shadow", "#moreActions");
+    moreAction.click();
+    const span = browser.custom$("shadow", ".dropdown-item:not([hidden])");
+    const text = span.getText();
+    // prints `Open downloads folder`
+    console.log("found:", text);
 
-//     browser.url("chrome://downloads/");
-//     browser.maximizeWindow();
-//     browser.pause(3000);  
-
-//     const moreActions = querySelectorDeep('#moreActions');
-//     console.log(moreActions);
-//     moreActions.click();
-
-//     browser.pause(3000);
-
-//     const span =  querySelectorDeep('.dropdown-item:not([hidden])');
-//     const text = span.getText();
-//     // prints `Open downloads folder`
-//     console.log(text);
-
-//     browser.deleteSession();
-//   });
-// });
+    //browser.deleteSession();
+  });
+});
